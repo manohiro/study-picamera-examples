f03d84a4ef27a8bf6e2097c0a60d3cb5e95a8e7e
manohiro <42936563+manohiro@users.noreply.github.com>
2018-12-13 19:43:55 +0900

    Update face_detector.py

--- camera/processor/face_detector.py
+++ camera/processor/face_detector.py
@@ -42,7 +42,7 @@ class FaceDetector(object):
         for (x,y,w,h) in faces:
             i += 1
             cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
-            cv2.putText(frame,'str(i)',(x,y), font, 4,(255,255,255),2,cv2.LINE_AA)
+            #cv2.putText(frame,'str(i)',(x,y), font, 4,(255,255,255),2,cv2.LINE_AA)
 
         # frameを戻り値として返す
         return frame
